6669782b2d098417316e1a20559c8b2aebac79d3
Adam T <adam.tizzone@gmail.com>
2017-12-01 22:40:13 +1100

    Removed URL column and made the project names clickable links (using the removed urls)

--- src/JobTable.js
+++ src/JobTable.js
@@ -13,7 +13,6 @@ export default class JobTable extends React.Component {
 						<th>Project</th>
 						<th>Description</th>
 						<th>Language</th>
-						<th>Link</th>
 					</tr>
 				</thead>
 				<tbody>
@@ -23,10 +22,9 @@ export default class JobTable extends React.Component {
 							<tr key={index}>
 								<th scope="row">{index}</th>
 								<td>{item.provider}</td>
-								<td>{item.name}</td>
+								<td><a href={item.url}>{item.name}</a></td>
 								<td>{item.description}</td>
 								<td>{item.language}</td>
-								<td>{item.url}</td>
 							</tr>
 						)
 					})
