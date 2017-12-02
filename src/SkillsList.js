9af45b6847d17253f434ff976d9ac9cad1b3b81f
Adam T <adam.tizzone@gmail.com>
2017-12-02 12:05:48 +1100

    Further UI improvements?

--- src/SkillsList.js
+++ src/SkillsList.js
@@ -11,6 +11,7 @@ export default class SkillsList extends React.Component {
 	}
 
 	handleChange(event) {
+                console.log(event.target.name);
 		this.setState({
 			[event.target.name]: !this.state[event.target.name]
 		})
@@ -30,29 +31,32 @@ export default class SkillsList extends React.Component {
 	}
 
 	render() {
+		console.log(this.state)
 		return (
-			<Container>
+			<Container style={{ textAlign: "center" }}>
 				<Label>Select the skills you wish to look for jobs in</Label>
-				<div style={{ padding: "10px"}}>
+				<div style={{ padding: "10px", textAlign: "center"}}>
 				{this.props.keywords.map((word, index) => {
 					return (
                                             <div style={{ padding: "5px", display: "inline-block"}}>
-						<div key={index} style={{ borderStyle: "solid", borderWidth: "1px", borderRadius: "3px", textSize:"1em", borderColor: BLUE_BORDER, color: "white", display: "inline-block", padding: "5px"}} 
+						<div key={index} style={{ borderStyle: "solid", borderWidth: "1px", borderRadius: "3px", textSize:"1em", borderColor: BLUE_BORDER, color: "white", display: "inline-block", padding: "5px", width: "200px"}} 
                                                   className="checkboxblue"
                                                 >	
 							<div style={{ textAlign: "center"}} className="co">
 								<Input 
                                                                   name={word} type="checkbox" key={index} onClick={(event) => this.handleChange(event)}
-                                                                  style={{ display: "none"}}
+                                                                  style={{ }}
                                                                 />
-								{word}
+								{word.toUpperCase()}
 							</div>
 						</div>	
                                             </div>	
 					)
 				})}
 				</div>
-				<Button onClick={() => this.passToFreelancer(this.state)} style={BUTTON_STYLE}>Find jobs</Button>
+				<Button onClick={() => this.passToFreelancer(this.state)} 
+                                  style={{ borderStyle: "solid", borderWidth: "1px", borderRadius: "9px", height: "4em", borderColor: BLUE_BORDER, backgroundColor: BLUE_BORDER, color: "white", width: "75%"}}
+                                >Find jobs</Button>
 			</Container>
 		)
 	}
