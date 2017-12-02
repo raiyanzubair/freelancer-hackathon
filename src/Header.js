a87eda6e18be5f301378b8119c40cde5d9c1f814
saus-75 <georgechieng@gmail.com>
2017-12-02 12:31:09 +1100

    image darken, font changes

--- src/Header.js
+++ src/Header.js
@@ -5,8 +5,8 @@ import { BLUE_BORDER } from './consts'
 export default class Header extends React.Component {
 	render() {
 		return (
-			<Jumbotron style={{background:BLUE_BORDER, margin: "auto", textAlign: "center", maxWidth: "800px"}}>
-				<h1 style={{color: "white", fontFamily: "Abril Fatface"}} className="display-3">grunter</h1>
+			<Jumbotron style={{background:BLUE_BORDER, backgroundImage: `url("table-blur.jpg")`, backgroundSize: 'cover', margin: "auto", textAlign: "center", maxWidth: "800px"}}>
+				<h1 style={{color: "white", fontFamily: "Lato, Sans-serif", fontWeight: "900"}} className="display-3">Occupo</h1>
 			</Jumbotron>
 		)
 	}
